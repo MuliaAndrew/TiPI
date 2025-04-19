181917d09d0052b479780ecae79dc7c61d11e0b9
MuliaAndrew <yerdna801@gmail.com>
2025-04-19 18:44:35 +0300

    DB implement key-value on-filesystem storage

--- DB/internal/kvstorage/kvs_api.go
+++ DB/internal/kvstorage/kvs_api.go
@@ -1,4 +1,4 @@
-package kvstorage;
+package kvstorage
 
 import (
 	"github.com/recoilme/pudge"
@@ -8,26 +8,40 @@ import (
 // we only need a single table in which we will store all data
 // and CRUD operations on it
 type Kvs struct {
-	db			pudge.Db
-	
+	db*			pudge.Db
+	path		string
 }
 
-func (kvs *Kvs) Open() error {
+func (kvs *Kvs) Open(path string) error {
+	kvs.path = path
+	cfg := new(pudge.Config)
+	cfg.SyncInterval = 5
+	cfg.StoreMode = 1
+
+	db, err := pudge.Open(path, cfg)
+	if err != nil {
+		return err
+	}
+	kvs.db = db
 	return nil
 } 
 
-func (kvs *Kvs) Write() error {
-	return nil
+func (kvs *Kvs) Write(key any, value any) error {
+	return kvs.db.Set(key, value)
 } 
 
-func (kvs *Kvs) Read() error {
-	return nil
+func (kvs *Kvs) Read(key any) (string, error) {
+	val := new(string)
+	err := kvs.db.Get(key, val)
+	return *val, err
 } 
 
-func (kvs *Kvs) Delete() error {
-	return nil
+func (kvs *Kvs) Delete(key any) error {
+	return kvs.db.Delete(key)
 } 
 
 func (kvs *Kvs) Close() error {
-	return nil
+	err := kvs.db.Close()
+	kvs.db = nil
+	return err
 } 
